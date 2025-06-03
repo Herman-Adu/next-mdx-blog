d8cd019555b5db458582ce37ac0b8cbecfb5fc61
Herman Adu <herman.adu@hotmail.com>
2025-06-03 20:33:12 +0100

    added container component and added top cagegories and popular post to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
+import Container from "@/components/Container";
 import LatestPost from "@/components/home/latest-post";
+import PopularPosts from "@/components/home/popular-posts";
+import TopCatogories from "@/components/home/top-categories";
 import { MainNav } from "@/components/main-nav";
 
 export default function Home() {
   return (
-    <>
+    <Container>
       <MainNav />
 
-      <main>
+      <main className="flex flex-col items-start justify-evenly mt-16 md:flex-row">
         <div>
           <LatestPost />
         </div>
+
+        <div className="h-screen">
+          <div>
+            <h1 className="font-bold mb-4">TOP CATEGORIES</h1>
+            <TopCatogories />
+          </div>
+
+          <div className="mt-10 sticky top-0">
+            <h1 className="font-bold mb-4">POPULAR POSTS</h1>
+            <PopularPosts />
+          </div>
+        </div>
       </main>
-    </>
+    </Container>
   );
 }
