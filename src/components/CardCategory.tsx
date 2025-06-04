53c62bfb7b3f3046451fc068933659d260467f26
Herman Adu <herman.adu@hotmail.com>
2025-06-04 14:35:03 +0100

    added dynamic categories page and a card cpmponent to show blogs, refactored top categories to use constants data

---
+++ src/components/CardCategory.tsx
@@ -0,0 +1,36 @@
+import { formatDate } from "@/app/blog/utils";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+
+interface CardCategoryProps {
+  title: string;
+  summary: string;
+  date: string;
+}
+
+export default function CardCategory({
+  title,
+  summary,
+  date,
+}: CardCategoryProps) {
+  console.log("Title: ", title);
+  return (
+    <Card className="w-[350px] h-[290px] shadow-lg">
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{summary}</p>
+      </CardContent>
+      <CardFooter>
+        <p className="text-xs text-gray-500">{formatDate(date)}</p>
+      </CardFooter>
+    </Card>
+  );
+}
