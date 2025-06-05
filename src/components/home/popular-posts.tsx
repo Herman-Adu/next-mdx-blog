9e9ae7b655f9bdcaff836c67b821bf32322bff71
Herman Adu <herman.adu@hotmail.com>
2025-06-05 17:38:37 +0100

    added endpoint in api layer to get all blogs from database, added swr and used it in popular posts to fetch all blogs from the api layer

--- src/components/home/popular-posts.tsx
+++ src/components/home/popular-posts.tsx
@@ -1,19 +1,25 @@
 "use client";
 
-import { popularPosts } from "@/lib/placeholder-data";
+//import { popularPosts } from "@/lib/placeholder-data";
+import { fetcher, fetchUrl } from "@/lib/utils";
 import { Icons } from "../icons";
+import Link from "next/link";
+import useSWR from "swr";
 
 export default function PopularPosts() {
+  const { data, error, isLoading } = useSWR(fetchUrl, fetcher);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
   return (
     <ul className="overflow-auto">
-      {popularPosts.map((post, index) => (
-        <li
-          key={index}
-          className="flex items-center gap-2 group cursor-pointer py-2"
-        >
-          <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 group-hover:text-orange-400 transition-all " />
-          <p>{post.title}</p>
-        </li>
+      {data?.map((post: { category: string; slug: string; title: string }) => (
+        <Link href={`/blog/${post.category}/${post.slug}`} key={post.title}>
+          <li className="flex items-center gap-2 group cursor-pointer py-2">
+            <Icons.arrowRight className="h-6 w-6 group-hover:translate-x-1 group-hover:text-orange-400 transition-all" />
+            <p>{post.title}</p>
+          </li>
+        </Link>
       ))}
     </ul>
   );
